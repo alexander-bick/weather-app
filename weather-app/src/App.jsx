510e82942a54e6cc2b2ff0fafb765ba2ba6a8f95
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-06-12 09:49:26 +0200

    add files

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+import Form from "./components/form/index.jsx";
+import { uid } from "uid";
+
+function App() {
+  const [activities, setActivities] = useState("activities", { defaultValue: [], });
+
+  function handleAddActivity(newActivity) {
+    setActivities([...activities, { id: uid(), ...newActivity }])
+  }
+  return (
+    <>
+      
+      <Form onAddActivity={handleAddActivity}/> 
+    </>
+  )
+}
+
+export default App
