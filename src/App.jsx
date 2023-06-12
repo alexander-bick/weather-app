30d929684bb46389f8bc26ca82f5b72142e0e9e6
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-06-12 10:42:08 +0200

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import './App.css';
 import Form from "./components/form/index.jsx";
 import { uid } from "uid";
 /* import List from "./components/list/index.jsx"; */
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [activities, setActivities] = useState("activities", { defaultValue: [], });
+  const [activities, setActivities] = useLocalStorageState("activities", { defaultValue: [], });
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, { id: uid(), ...newActivity }])
