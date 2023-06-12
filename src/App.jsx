bff0d2c3513e769d939de864876019640ca6b583
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-06-12 10:18:27 +0200

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Form from "./components/form/index.jsx";
 import { uid } from "uid";
-import List from "./components/list/index.jsx";
+/* import List from "./components/list/index.jsx"; */
 
 function App() {
   const [activities, setActivities] = useState("activities", { defaultValue: [], });
@@ -12,7 +12,6 @@ function App() {
   }
   return (
     <>
-      <List />
       <Form onAddActivity={handleAddActivity}/> 
     </>
   )
